8da4550b44bb02d4c1987534a9eb554c590643a6
Tyler Waugh <waughphoto@gmail.com>
2022-05-07 12:48:54 -0400

    Workshop 1

---
+++ src/features/campsites/CampsiteCard.js
@@ -0,0 +1,15 @@
+import { Card, CardImgOverlay, CardImg, CardTitle } from 'reactstrap';
+
+const CampsiteCard = (props) => {
+    return (
+    <Card>
+        <CardImg width='100%' src={props.campsite.image} alt={props.campsite.name}/> 
+        
+        <CardImgOverlay>
+        <CardTitle>{props.campsite.name}</CardTitle>
+        </CardImgOverlay>
+    </Card>
+    )
+};
+
+export default CampsiteCard;
